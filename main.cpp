70f919ae7a683b16c46dcdc1ec37c216cf324f10
marko shenouda <136264452+Markoshenouda@users.noreply.github.com>
2025-04-26 20:22:55 +0300

    Create main.cpp
    main

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "include/constants.hpp"
+#include "include/utils.hpp"
+#include "include/board.hpp"
+#include "include/ai.hpp"
+#include "include/leaderboard.hpp"
+#include "include/menu.hpp"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <ctime>
+#include <limits>
+
+int main() {
+    #ifdef _WIN32
+        system("chcp 65001");
+        system("color 0D");
+    #endif
+
+    srand(time(NULL));
+    // ... rest of main game loop
+}
